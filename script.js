88fd0f1203baa510e80515db8e471871877ea82f
franklin-le <80184063+franklin-le@users.noreply.github.com>
2021-04-15 21:33:19 -0700

    added both volume bars and added ambient sounds

--- script.js
+++ script.js
@@ -1,6 +1,16 @@
-var shuffleIcon = document.getElementById("shuffleIcon");
 var nextSong = document.getElementById("nextSong");
-nextSong.volume = 0.1;
+
+var nextSound = document.getElementById("nextSound");
+
+var nextBG = document.getElementById("nextBG");
+
+var volume = document.querySelector('#volume');
+
+var volume2 = document.querySelector('#volume2');
+
+nextSong.volume = 0.5;
+
+nextSound.volume = 0.5;
 
 function shufflePlay() {
 
@@ -18,11 +28,24 @@ function shufflePlay() {
     nextSong.play();
 }
 
-shuffleIcon.onclick = function () {
-    shufflePlay();
+function soundPlay() {
+
+    soundArray = [
+        "sound.mp3",
+        "sound1.mp3",
+        "sound2.mp3"]
+
+    var num = Math.floor(Math.random() * soundArray.length);
+    nextSound.load();
+
+    nextSound.innerHTML = "";
+    nextSound.innerHTML += '<source src="media/' + soundArray[num] + '" type="audio/mp3">';
+
+
+    nextSound.play();
 }
 
-function pausePlay() {
+function pauseButton() {
     if (nextSong.paused) {
         nextSong.play();
     }
@@ -31,3 +54,48 @@ function pausePlay() {
     }
 
 }
+
+volume.addEventListener('change', function (e) {
+    nextSong.volume = e.currentTarget.value / 100;
+});
+
+volume2.addEventListener('change', function (e) {
+    nextSound.volume = e.currentTarget.value / 100;
+});
+
+
+window.onkeydown = function (event) {
+    if (event.keyCode == 32) {
+        shufflePlay();
+    }; 
+}; 
+
+
+
+/* Background Button */
+
+function shuffleBG(){
+
+    bgArray = [
+        "image.gif",
+        "image1.gif",
+        "image2.gif"
+    ]
+    
+    var num = Math.floor(Math.random() * bgArray.length);
+    nextBG.load();
+
+    nextBG = "";
+    nextBG += "url('media/" + bgArray[num] + "')";
+
+    document.body.style.backgroundImage = nextBG;
+}
+
+
+/*
+let bgButton = document.querySelector('#bgButton');
+
+bgButton.addEventListener('click', () =>{
+    document.body.style.backgroundImage = "url('" + nextBG + "')";
+})
+*/
